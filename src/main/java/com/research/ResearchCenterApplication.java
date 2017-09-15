85592667aafa832f1f1d06f8853c9a73e740f0fc
abdelrhman.galal <abdelrhman.tanga@gmail.com>
2017-09-15 21:36:32 +0200

    liquibase running

--- src/main/java/com/research/ResearchCenterApplication.java
+++ src/main/java/com/research/ResearchCenterApplication.java
@@ -1,12 +1,67 @@
 package com.research;
 
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import liquibase.integration.spring.SpringLiquibase;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @SpringBootApplication
 public class ResearchCenterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResearchCenterApplication.class, args);
+	}
+	
+	@Bean
+	public SpringLiquibase liquibase() throws PropertyVetoException {
+	    SpringLiquibase liquibase = new SpringLiquibase();
+	    liquibase.setChangeLog("classpath:liquibase/liquibase-changeLog.xml");
+	    liquibase.setDataSource(dataSource());
+	    liquibase.setDefaultSchema("research_center");
+	    return liquibase;
+	}
+	
+	@Bean
+	public ComboPooledDataSource dataSource() throws PropertyVetoException {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		dataSource.setDriverClass("org.postgresql.Driver");
+		dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/RESEARCHCENTER");
+		dataSource.setUser("postgres");
+		dataSource.setPassword("admin");
+		dataSource.setMaxPoolSize(10);
+		dataSource.setMinPoolSize(50);
+		dataSource.setMaxStatements(100);
+		return dataSource;
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws PropertyVetoException {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String[] { "model.persistence.entities" });
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+
+		return em;
+	}
+	
+	Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "none");
+		properties.setProperty("hibernate.dialect",
+				"org.hibernate.dialect.PostgreSQLDialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		return properties;
 	}
 }
--- src/main/java/com/research/controllers/ProjectController.java
+++ src/main/java/com/research/controllers/ProjectController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.research.dto.ProjectDto;
 
 @RestController
-@CrossOrigin("${angular.url}")
+//@CrossOrigin("${angular.url}")
 @RequestMapping("/project")
 public class ProjectController {
 
