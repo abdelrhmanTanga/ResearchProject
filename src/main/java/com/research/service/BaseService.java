e080ad701099751f0a94ab61cc4caf8726c861f7
abdelrhman.galal <abdelrhman.tanga@gmail.com>
2017-09-15 23:21:28 +0200

    base service

---
+++ src/main/java/com/research/service/BaseService.java
@@ -0,0 +1,17 @@
+package com.research.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseService<T, E, ID extends Serializable> {
+
+	public List<T> getAll();
+	
+	public T get(ID id);
+	
+	public T insert(E dto);
+	
+	public T update(E dto);
+	
+	public void delete(E dto);
+}
---
+++ src/main/java/com/research/service/BaseServiceImpl.java
@@ -0,0 +1,28 @@
+package com.research.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class BaseServiceImpl<T, E, ID extends Serializable> {
+
+	
+	public T get(){
+		return null;
+	}
+	
+	public List<T> getAll(Long id){
+		return null;
+	}
+	
+	public T update(E dto){
+		return null;
+	}
+	
+	public T insert(E dto){
+		return null;
+	}
+	
+	public void delete(E dto){
+		
+	}
+}
