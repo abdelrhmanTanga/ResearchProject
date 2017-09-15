70ce86db93fe555eb313ff857b1b9ed8b587dde9
mohamedaligaber <mohamed.aly.201412@gmail.com>
2017-09-15 20:51:33 +0200

    conflict

--- src/main/java/com/research/ResearchCenterApplication.java
+++ src/main/java/com/research/ResearchCenterApplication.java
@@ -1,12 +1,12 @@
 package com.research;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ResearchCenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ResearchCenterApplication.class, args);
+	//	SpringApplication.run(ResearchCenterApplication.class, args);
 	}
 }
