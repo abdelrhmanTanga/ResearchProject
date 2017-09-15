1787bec33a200027e21452fd0264fb6752170acf
Sherif <sherifahmed126@gmail.com>
2017-09-15 15:33:39 +0200

    pom

---
+++ src/main/java/com/research/ResearchCenterApplication.java
@@ -0,0 +1,12 @@
+package com.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchCenterApplication.class, args);
+	}
+}
